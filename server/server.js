b538e4a619e3550bee6fb080d4e1fe268e401fea
Igor Iglesias <rogih_95@hotmail.com>
2020-11-20 18:32:10 +0100

    :sparkles: Add DB initialization

--- server/server.js
+++ server/server.js
@@ -1,13 +1,20 @@
+/**
+ * Server start point
+ */
+
 const express = require('express');
 const app = express();
+
+const db = require('./db.js');
 
 app.use('/', express.static(__dirname));
 
 app.get('/', (req, res) => {
-    res.sendFile('index.html', {root: './client'})
-})
+    res.sendFile('index.html', {root: './client'});
+});
 
 const port = process.env.PORT || 8080;
-app.listen(port, function () {
+app.listen(port, async () => {
+    await db.startDB();
     console.log('Server listening on port ' + port);
 });
