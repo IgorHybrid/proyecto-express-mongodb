9095bee92d321e5931c801efdd2eb9cd37b40cdf
Igor Iglesias <rogih_95@hotmail.com>
2020-11-21 15:36:32 +0100

    :sparkles: Add error handler

--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ const bodyParser = require('body-parser')
 const cors = require('cors');
 
 const db = require('./db.js');
+const error404Handler = require('./middleware/error404');
+const errorHandler = require('./middleware/error-handler');
 
 app.use('/', express.static(__dirname));
 app.use(express.static('public'))
@@ -21,6 +23,10 @@ app.use(cors());
 app.use('/', require('./routes/home'));
 app.use('/types', require('./routes/type'));
 app.use('/request', require('./routes/request'));
+
+//Error middleware
+app.use(error404Handler);
+app.use(errorHandler);
 
 const port = process.env.PORT || 8080;
 app.listen(port, async () => {
