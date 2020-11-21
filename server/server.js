d769fd8f8131bfefbdfc4711231d7bf21a41968b
Igor Iglesias <rogih_95@hotmail.com>
2020-11-21 13:04:53 +0100

    :heavy_plus_sign: Add dependencies

--- server/server.js
+++ server/server.js
@@ -4,10 +4,17 @@
 
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
+const cors = require('cors');
 
 const db = require('./db.js');
 
 app.use('/', express.static(__dirname));
+
+app.use(bodyParser.json());
+
+//Enable all CORS requests
+app.use(cors());
 
 app.use('/', require('./routes/home'));
 app.use('/types', require('./routes/type'));
